682dae59f5a754a7c736d3dd82e46a78b9813594
unknown <nghia.buidai.2005@gmail.com>
2024-05-24 20:57:18 +0700

    Project-3

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      screens: {
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1200px',
+        '2xl': '1400px',
+      },  
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary: '#000000',
+        }
+      },
+      fontFamily: {
+        pacifico: ['Pacifico', 'cursive'],
+        anton: ['Anton', 'sans-serif'],
+        freeman: ['Freeman', 'sans-serif'],
+      }
+    }
+  }
